15b609b26f993b707d505228f2ee01a3a3590ce7
GraniLuk <granica.lukasz@gmail.com>
2025-04-04 20:28:26 +0200

    Add Microsoft.Extensions.Http And move to net9

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Http;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
